2afdaf836d0655567e0b633c6a4f76adda640444
Chris Jungerius <37599089+cjungerius@users.noreply.github.com>
2022-02-15 12:16:57 +0000

    added experiment logic and edited display_functions.py (#6)
    * added experiment design logic
    
    * rewrite of display functions, cross functionality with experiment.py

--- scripts/experiment.py
+++ scripts/experiment.py
@@ -1,30 +1,31 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# The first line is called the hash bang. It helps operating systems 
-# understand what to do with this script. The second line describes 
-# the encoding of this file. UTF-8 is the only one used nowadays.
-# It supports all alphabeths, even runes and linear B.
- 
-# Import the PsychoPy libraries that you want to use
-from psychopy import core, visual
- 
-# Create a window
-win = visual.Window([400,300], monitor="testMonitor")
- 
-# Create a stimulus for a certain window
-message = visual.TextStim(win, text="Hello World!")
- 
-# Draw the stimulus to the window. We always draw at the back buffer of the window.
-message.draw()
- 
-# Flip back buffer and front  buffer of the window.
-win.flip()
- 
-#a Pause 5 s, so you get a chance to see it!
-core.wait(5.0)
- 
-# Close the window
-win.close()
- 
-# Close PsychoPy
-core.quit()
+from psychopy import core, data
+from display_functions import *
+
+def run():
+
+	# Create factorial design
+	factors = {
+		'targetloc': [0, 2, 4, 6],
+		'distractor': [True, False],
+		'trialType': ['search', 'search', 'probe'],
+		'lineDir': ['l','r']
+	}
+
+	trialList = data.createFactorialTrialList(factors)
+	
+	trials = data.TrialHandler(trialList,2, method='random')
+
+	# Run the experiment
+	for trial in trials:
+		print(trial)
+		drawFixationDisplay(mywin)
+		drawSearchDisplay(mywin)
+
+	# Close the window
+	mywin.close()
+	
+	# Close PsychoPy
+	core.quit()
+
+if __name__ == '__main__':
+	run()
