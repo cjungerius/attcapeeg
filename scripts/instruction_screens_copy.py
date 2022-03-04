1ca52becb643b5abea7dd2c797e6bf8e3ede4932
Chris Jungerius <37599089+cjungerius@users.noreply.github.com>
2022-03-04 15:28:11 +0100

    update instruction_screens_copy

--- scripts/instruction_screens_copy.py
+++ scripts/instruction_screens_copy.py
@@ -8,68 +8,51 @@ dir = os.path.dirname(path)
 dir = dir.replace('scripts', 'media')
 os.chdir(dir)
 
-def drawBoxesWelcome(): 
-    polygon_next = visual.Rect(mywin, width = 0.14, height = 0.1, pos = (.1,-.2), ori=0,
-    lineWidth=1, lineColor='black', lineColorSpace='rgb',
+def drawBoxes(x,y): 
+    # position: (.1, -.2) or (.1, -.6)
+    polygon_next = visual.Rect(mywin, width = 0.14, height = 0.1, pos = (x,y), ori=0,
+    lineWidth=1, lineColor=[0.6549, 0.6549, 0.6549], lineColorSpace='rgb',
     fillColor=[1,1,1], fillColorSpace='rgb',
     opacity=1, interpolate=True)
-    
-    polygon_prev = visual.Rect(mywin, width = 0.18, height = 0.1, pos = (-.1,-.2), ori=0,
-    lineWidth=1, lineColor='black', lineColorSpace='rgb',
-    fillColor=[1,1,1], fillColorSpace='rgb',
-    opacity=1, interpolate=True)
-    
-    next_text = visual.TextStim(mywin,
-    text='Next >',
-    font='Arial',
-    pos=(.1,-.2), height=0.06, wrapWidth=None, ori=0,
-    color='black', colorSpace='rgb', opacity=1)
-    
-    prev_text = visual.TextStim(mywin,
-    text='< Previous',
-    font='Arial',
-    pos=(-.1,-.2), height=0.06, wrapWidth=None, ori=0,
-    color='black', colorSpace='rgb', opacity=1)
-    
-    return polygon_next, polygon_prev, next_text, prev_text
 
-def drawBoxesScreens(): 
-    polygon_next = visual.Rect(mywin, width = 0.14, height = 0.1, pos = (.1,-.6), ori=0,
-    lineWidth=3, lineColor=[0.6549, 0.6549, 0.6549], lineColorSpace='rgb',
-    fillColor=[1,1,1], fillColorSpace='rgb',
-    opacity=1, interpolate=True)
+    polygon_next.draw()
     
-    polygon_prev = visual.Rect(mywin, width = 0.18, height = 0.1, pos = (-.1,-.6), ori=0,
-    lineWidth=3, lineColor=[0.6549, 0.6549, 0.6549], lineColorSpace='rgb',
+    polygon_prev = visual.Rect(mywin, width = 0.18, height = 0.1, pos = (-x,y), ori=0,
+    lineWidth=1, lineColor=[0.6549, 0.6549, 0.6549], lineColorSpace='rgb',
     fillColor=[1,1,1], fillColorSpace='rgb',
     opacity=1, interpolate=True)
+
+    polygon_prev.draw()
     
     next_text = visual.TextStim(mywin,
     text='Next >',
     font='Arial',
-    pos=(.1,-.6), height=0.06, wrapWidth=None, ori=0,
+    pos=(x,y), height=0.06, wrapWidth=None, ori=0,
     color='black', colorSpace='rgb', opacity=1)
+
+    next_text.draw()
     
     prev_text = visual.TextStim(mywin,
     text='< Previous',
     font='Arial',
-    pos=(-.1,-.6), height=0.06, wrapWidth=None, ori=0,
+    pos=(-x,y), height=0.06, wrapWidth=None, ori=0,
     color='black', colorSpace='rgb', opacity=1)
+
+    prev_text.draw()
     
-    return polygon_next, polygon_prev, next_text, prev_text
+    return polygon_next, polygon_prev
+
 
 
 def instructionScreensPictures(mywin, target_shape = 'c', target_colour = 'r'): 
     i = 0 # initate start val
     mouse = event.Mouse(win=mywin)
 
-    pn, pp, nt, pt = drawBoxesWelcome()
+    pn, pp = drawBoxes(.1,-.2)
     inst_text = visual.TextStim(mywin, 'Welcome to the experiment! We will instruct you to the task you will preform'
                                     , units = "pix")
-    pn.draw()
-    pp.draw()
-    nt.draw()
-    pt.draw()
+
+
 
     inst_text.color = 'black'
     inst_text.height = 20
@@ -100,24 +83,14 @@ def instructionScreensPictures(mywin, target_shape = 'c', target_colour = 'r'):
                     img = visual.ImageStim(mywin, image='inst_search_'+ target_colour +'.png', units="pix")
                     img.size = img.size*0.3
                 if i == 3: 
-                    inst_text = visual.TextStim(mywin, text = 'Your task is to search for the               , then react to the line segment inside it. If the line segment points to the left, press the left arrow key; if the line segment points to the right, press the right arrow key.', units="pix")
+                    inst_text = visual.TextStim(mywin, text = 'Your task is to search for the *' + ("diamond" if target_shape == 'd' else "circle")+ '*, then react to the line segment inside it. If the line segment points to the left, press the left arrow key; if the line segment points to the right, press the right arrow key.', units="pix")
                     img = visual.ImageStim(mywin, image='inst_search_'+ target_colour + target_shape +'.png', units="pix")
                     img.size = img.size*0.4
                     
-                    # if we want to make this work I need to know the exact screen size of our expi otherwise cannot make it bold 
-                    if target_shape == 'd':
-                        g_text = visual.TextStim(mywin, text = 'diamond', units = "pix", bold = True)
-                    
-                    if target_shape == 'c':
-                        g_text = visual.TextStim(mywin, text = 'circle', units = "pix", bold = True)
-                        
-                    g_text.pos = (0+mywin.size[0]/37,-mywin.size[1]/13)
-                    g_text.color = 'black'
-                    g_text.height = 20
                     
                 inst_text.pos = (0,-mywin.size[1]/10)
         
-                pn, pp, nt, pt = drawBoxesScreens()
+                pn, pp = drawBoxes(.1,-.6)
                 
                 img.pos = (0, mywin.size[1]/13)
         
@@ -138,22 +111,16 @@ def instructionScreensPictures(mywin, target_shape = 'c', target_colour = 'r'):
             
             elif i == 4:
                 inst_text = visual.TextStim(mywin, 'Please try to be as fast and accurate as possible throughout the experiment!', units = "pix")
-                pn, pp, nt, pt = drawBoxesWelcome()
+                pn, pp = drawBoxes(.1,-.2)
 
             elif i == 5: 
                 inst_text = visual.TextStim(mywin, 'We will now begin with a brief practice run so you can get acquainted with the task. Press Next to start the practice run!', units = "pix")
-                pn, pp, nt, pt = drawBoxesWelcome()
+                pn, pp = drawBoxes(.1,-.2)
 
             inst_text.color = 'black'
             inst_text.height = 20
             inst_text.draw()
-            if i == 3: # needs to be here bc first the text with the space needs to be drawn where then the bold text of the starget shape can be drawn onto
-                g_text.draw()
-            
-            pn.draw()
-            pp.draw()
-            nt.draw()
-            pt.draw()
+
             mywin.flip()
 
 
