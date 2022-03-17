67f7dd9ae0dc277f65d3fa6877eac293e5d204d4
Chris Jungerius <37599089+cjungerius@users.noreply.github.com>
2022-03-17 15:55:21 +0100

    add mapper trials to main experiment

--- scripts/experiment.py
+++ scripts/experiment.py
@@ -8,6 +8,19 @@ def run():
 	info = get_subject_info(1234)
 
 # Create factorial design
+
+	mapperfactors = {
+		'targetloc': [0,1,2,3,4,5,6,7]
+	}
+
+	mapperTrialList = data.createFactorialTrialList(mapperfactors)
+	mappertrials = data.TrialHandler(mapperTrialList, 10, method='random')
+
+	for trial in mappertrials:
+		print(trial)
+		g = mapper_trial(trial)
+		print(g)
+
 	factors = {
 		'targetloc': [0, 2, 4, 6],
 		'distractor': [True, False],
