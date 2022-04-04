7525e59bcac0e2c171a058068f6130e5bbc8925d
sophie-pz <98156621+sophie-pz@users.noreply.github.com>
2022-04-04 09:00:49 +0200

    changed to blocks 8 search and 8/4 search
    Added a for loop so that now blocks are created that either first the 8 search or first the 8/4 search appears (randomly; could be that we want to store that info in th subjects?).

--- scripts/experiment.py
+++ scripts/experiment.py
@@ -7,24 +7,35 @@ def run():
 
 	info = get_subject_info(1234)
 
-# Create factorial design
-	factors = {
-		'targetloc': [0, 2, 4, 6],
-		'distractor': [True, False],
-		'trialType': ['search', 'search', 'probe'],
-		'lineDir': ['l','r'], 
-		'novel': [True, False]
-	}
+# either like this or it is given by the subject info..
+	blockTypes = ['search 8', 'search 8/4']
+	random.shuffle(blockTypes)
 
-	trialList = data.createFactorialTrialList(factors)
+	for blockType in blockTypes:
+		targetLocs = [0,1,2,3,4,5,6,7]
+		print(blockType)
 
-	trials = data.TrialHandler(trialList,2, method='random')
+		if blockType == 'search 8/4': 
+			targetLocs = [0,2,4,6]  * 2 # to get same trails num per block?
 
-	# Run the experiment
-	for trial in trials:
-		print(trial)
-		g = general_trial(trial, info)
-		print(g)
+
+		factors = {
+			'distractor': [True, False],
+			'targetloc': targetLocs, # now store block type but no longer target loc
+			'trialType': ['search', 'search', 'probe'],
+			'lineDir': ['l','r'], 
+		}
+
+		trialList = data.createFactorialTrialList(factors)
+
+		trials = data.TrialHandler(trialList,2, method='random')
+
+		# Run the experiment
+		for trial in trials:
+			print(trial)
+			g = general_trial(trial, info, blockType)
+			print(g)
+
 	# Close the window
 	mywin.close()
 
